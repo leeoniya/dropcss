c8790a0897d0c92ef9e3900f47d85c5ab95e7d99
Leon Sorokin <leeoniya@gmail.com>
2019-03-25 20:29:04 -0500

    prune text nodes from HTML AST (+23% perf). poke #1.

--- src/dropcss.js
+++ src/dropcss.js
@@ -1,4 +1,4 @@
-const { parse } = require('node-html-parser');
+const { parse, TextNode } = require('node-html-parser');
 const CSSTree = require('css-tree');
 const CSSselect = require("css-select");
 const adapter = require("css-select-browser-adapter");
@@ -8,8 +8,26 @@ const pseudoClassNonTransient = /:(?:first|last|nth|only|not|empty)\b/;		// |lan
 
 const doDrop = sel => true;
 
+// removes text nodes from HTML AST
+function pruneText(node) {
+	if (node.childNodes) {
+		node.childNodes = node.childNodes.filter(ch => {
+			if (ch instanceof TextNode)
+				return false;
+
+			pruneText(ch);
+			return true;
+		});
+	}
+
+	return node;
+}
+
 function dropcss(opts) {
-	const htmlAst = parse(opts.html);
+	let htmlAst = parse(opts.html);
+
+	if (!opts.keepText)
+		pruneText(htmlAst);
 
 	const cssAst = CSSTree.parse(opts.css, {
 		parseRulePrelude: false,
