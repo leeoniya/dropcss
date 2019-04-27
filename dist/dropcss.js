ce91f2cd95067661c7bbf499618c38b77c6ea253
Leon Sorokin <leeoniya@gmail.com>
2019-04-26 19:32:47 -0500

    1.0.7

--- dist/dropcss.js
+++ dist/dropcss.js
@@ -4,7 +4,7 @@
 *
 * dropcss.js (DropCSS)
 * An exceptionally fast, thorough and tiny unused-CSS cleaner
-* https://github.com/leeoniya/dropcss (v1.0.7-dev)
+* https://github.com/leeoniya/dropcss (v1.0.7)
 */
 
 (function (global, factory) {
